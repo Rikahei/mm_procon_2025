7c8c4be4a08efba7c9fdfaaf9bfadc1e26ec665e
rikahei <rikaheijp@gmail.com>
2025-05-05 02:32:04 +0900

    Fix star position

--- app/frontend/src/main.ts
+++ app/frontend/src/main.ts
@@ -47,38 +47,34 @@ async function main (){
 	cylinder.rotation.x = Math.PI / 2;
 
 	scene.add( cylinder );
-	// skyObjects.push( cylinder );
 
-	const boxWidth = 3;  
-	const boxHeight = 3;  
-	const boxDepth = 3; 
-	const boxGeo = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+	// Moon
+	const moonGeo = new THREE.DodecahedronGeometry(5, 3);
 
-	const boxMaterial = new THREE.MeshPhongMaterial({color: 'white'});
-	const Box = new THREE.Mesh( boxGeo, boxMaterial );
-	Box.position.x = 0;
-	Box.position.y = -100;
-	Box.position.z = -20;
-	Box.rotation.x = Math.PI / 2;
+	const moonMaterial = new THREE.MeshPhongMaterial({color: 'yellow'});
+	const moon = new THREE.Mesh( moonGeo, moonMaterial );
+	moon.position.x = 0;
+	moon.position.y = -85;
+	moon.position.z = -20;
 
-	skySystem.add( Box );
-	skyObjects.push( Box );
+	skySystem.add( moon );
+	skyObjects.push( moon );
 
 	const starsGeo = new THREE.DodecahedronGeometry(0.1, 1);
-	const starsSpace = 150;
-	const spaceFix = 20;
+	const starsSpace = 220;
+	const spaceFix = 110;
 	for (let i = 0; i < 200; i++) {
 		const star = new THREE.Mesh(starsGeo, new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff }));
 		
 		star.position.x = Math.random() * starsSpace - spaceFix;
 		star.position.y = Math.random() * starsSpace - spaceFix;
-		star.position.z = Math.random() * starsSpace - spaceFix - 20;
+		star.position.z = Math.random() * starsSpace - spaceFix;
 
 		star.rotation.x = Math.random() * 2 * Math.PI;
 		star.rotation.y = Math.random() * 2 * Math.PI;
 		star.rotation.z = Math.random() * 2 * Math.PI;
 
-		star.scale.x, star.scale.y, star.scale.z = Math.random() + 1.5;
+		star.scale.x, star.scale.y, star.scale.z = Math.random() * 2 + 3;
 		
 		skySystem.add(star);
 		skyObjects.push( star );
