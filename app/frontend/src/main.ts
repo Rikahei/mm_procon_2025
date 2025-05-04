c0cfe7a82400d9e956fba8000283e5900f3644ea
rikahei <rikaheijp@gmail.com>
2025-05-04 21:02:44 +0900

    Add star and box

--- app/frontend/src/main.ts
+++ app/frontend/src/main.ts
@@ -11,13 +11,13 @@ async function main (){
 
 	const fov = 120;
 	const aspect = 2; // the canvas default
-	const near = 1;
-	const far = 10;
+	const near = 0.1;
+	const far = 50;
 	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
 	camera.position.z = 10;
 
 	const scene = new THREE.Scene();
-
+	const objects = [];
 	{
 
 		const color = 0xFFFFFF;
@@ -28,32 +28,59 @@ async function main (){
 
 	}
 
-	const radiusTop = 22;
-	const radiusBottom = 22;  
+	const radiusTop = 30;
+	const radiusBottom = 30;  
 	const height = 6;  
 	const radialSegments = 60;
 
 	const geometry = new THREE.CylinderGeometry(
-		radiusTop, radiusBottom, height, radialSegments );
+	radiusTop, radiusBottom, height, radialSegments );
+	const material = new THREE.MeshPhongMaterial({color: 0x44aa88});
+	const cylinder = new THREE.Mesh( geometry, material );
+	cylinder.position.x = 0;
+	cylinder.position.y = -40;
+	cylinder.rotation.x = Math.PI / 2;
 
-	function makeInstance( geometry, color ) {
+	scene.add( cylinder );
+	objects.push( cylinder );
 
-		const material = new THREE.MeshPhongMaterial( { color } );
+	const boxWidth = 8;  
+	const boxHeight = 8;  
+	const boxDepth = 8; 
+	const boxGeo = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-		const cylinder = new THREE.Mesh( geometry, material );
-		scene.add( cylinder );
+	const boxMaterial = new THREE.MeshPhongMaterial({color: 'white'});
+	const Box = new THREE.Mesh( boxGeo, boxMaterial );
+	Box.position.x = 0;
+	Box.position.y = -20;
+	Box.position.z = -50;
+	Box.rotation.x = Math.PI / 2;
 
-		cylinder.position.x = 0;
-		cylinder.position.y = -30;
-		cylinder.rotation.x = Math.PI / 2;
+	cylinder.z = -50;
 
-		return cylinder;
+	cylinder.add( Box );
+	objects.push( Box );
 
+	const starsGeo = new THREE.DodecahedronGeometry(0.1, 1);
+	const starsSpace = 150;
+	const spaceFix = 20;
+	for (let i = 0; i < 200; i++) {
+		const star = new THREE.Mesh(starsGeo, new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff }));
+		
+		star.position.x = Math.random() * starsSpace - spaceFix;
+		star.position.y = Math.random() * starsSpace - spaceFix;
+		star.position.z = Math.random() * starsSpace - spaceFix;
+
+		star.rotation.x = Math.random() * 2 * Math.PI;
+		star.rotation.y = Math.random() * 2 * Math.PI;
+		star.rotation.z = Math.random() * 2 * Math.PI;
+
+		star.scale.x, star.scale.y, star.scale.z = Math.random() + 1.5;
+		
+		cylinder.add(star);
+		objects.push( star );
 	}
 
-	const cylinders = [
-		makeInstance( geometry, 0x44aa88 ),
-	];
 
 	function resizeRendererToDisplaySize( renderer ) {
 
@@ -74,7 +101,7 @@ async function main (){
 
 	function render( time ) {
 
-		time *= 0.0001;
+		time *= 0.001;
 
 		if ( resizeRendererToDisplaySize( renderer ) ) {
 
@@ -84,12 +111,8 @@ async function main (){
 
 		}
 
-		cylinders.forEach( ( cylinder, ndx ) => {
-			const speed = 1 + ndx * .1;
-			const rot = time * speed;
-			// cylinder.rotation.x = rot;
-			cylinder.rotation.y = rot;
-
+		objects.forEach( ( obj ) => {
+			obj.rotation.y = time * 0.3;
 		} );
 
 		renderer.render( scene, camera );
