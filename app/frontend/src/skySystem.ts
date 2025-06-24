49e91a14814fd197f8c5597eb7cbedf000552f73
rikahei <rikaheijp@gmail.com>
2025-06-25 02:23:12 +0900

    Add play button

--- app/frontend/src/skySystem.ts
+++ app/frontend/src/skySystem.ts
@@ -9,15 +9,15 @@ skyObjects.push( skySystem );
 
 // Stars
 const starsGeo = new THREE.IcosahedronGeometry(1, 0);
-const starsSpace = 300;
+const starsSpace = 260;
 const spaceFix = 100;
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 120; i++) {
     const starsMaterial = new THREE.MeshBasicMaterial({ color: Math.random() * 0xffffff });
     const star = new THREE.Mesh(starsGeo, starsMaterial);
     star.layers.enable( 1 );
     star.position.x = Math.random() * starsSpace - spaceFix;
     star.position.y = Math.random() * starsSpace - spaceFix;
-    star.position.z = Math.random() * starsSpace - spaceFix - 150;
+    star.position.z = Math.random() * starsSpace - spaceFix - 180;
 
     star.rotation.x = Math.random() * 2 * Math.PI;
     star.rotation.y = Math.random() * 2 * Math.PI;
